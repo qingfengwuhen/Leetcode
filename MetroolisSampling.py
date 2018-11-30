e1bbf488aed05eb88490009cbecb8082bc365a12
qingfengwuhen <lingyu.666.happy@163.com>
2018-11-30 10:06:33 +0800

    2018-11-30-2

---
+++ MetroolisSampling.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created by Zhao Baoxin on 6/25/18
+-------------------------------------------------
+   File Name：     MetroolisSampling
+   Description :
+   Author :       zhaobx
+   date：          6/25/18
+-------------------------------------------------
+   Change Activity:
+                   6/25/18:
+-------------------------------------------------
+"""
+
